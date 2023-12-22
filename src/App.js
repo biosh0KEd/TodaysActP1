222a2fc548a7fbfb38038cf591d9d08dc27046d7
Efrain Giron <efrainbond@hotmail.com>
2023-12-22 17:53:41 -0600

    Terminada la funcionalidad de los TODOS

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
+import React from 'react';
 
 const defaultTodos = [
   { text: 'Cortar cebolla', completed: true },
@@ -12,16 +13,45 @@ const defaultTodos = [
 ]
 
 function App() {
+  const [searchValue, setSearchValue] = React.useState('');
+  const [todos, setTodos] = React.useState(defaultTodos);
+
+  const completedTodos = todos.filter(todo => !!todo.completed).length; // !! = Boolean
+  const totalTodos = todos.length;
+
+  const searchedTodos = todos.filter(todo => 
+    todo.text.toLowerCase().includes(searchValue.toLowerCase())
+  );
+
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+  }
+
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  }
+
   return (
     <>
-      <TodoCounter completed={16} total={25} />
-      <TodoSearch />
+      <TodoCounter completed={completedTodos} total={totalTodos} />
+      <TodoSearch searchValue={searchValue}
+                  setSearchValue={setSearchValue}/>
       <TodoList>
-        {defaultTodos.map(todo => 
+        {searchedTodos.map(todo => 
           <TodoItem 
             key={todo.text} 
             text={todo.text} 
-            completed={todo.completed}/>)}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+            />            
+        )}            
       </TodoList>
 
       <CreateTodoButton />
